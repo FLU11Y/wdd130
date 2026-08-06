4c8591f6bd0b1a8596393670ffc5e3d8ece8d7c1
Paulo Mateus <pawllo.raphael@outlook.com>
2026-08-05 21:21:57 -0300

    commit all

--- Week05/CSE110 Code Along.py
+++ Week05/CSE110 Code Along.py
@@ -9,8 +9,6 @@ while number != 0:
     if number != 0:
         numbers.append(number)
 
-# The list "numbers" now has all the numbers the user typed
-
 # Step 1: Find the sum or total
 sum = 0
 
@@ -20,7 +18,7 @@ for number in numbers:
 print(f"The sum is: {sum}")
 
 # Step 2: Find the average
-# We can use the sum we just computed...
+
 count = len(numbers)
 
 if count == 0:
@@ -34,28 +32,19 @@ else:
     print(f"The average is: {average}")
 
     # Step 3: Find the max
-    # We will walk through the numbers again, this time keeping track
-    # of the best so far, or the highest number to that point.
 
     best_so_far = numbers[0]
 
     for number in numbers[1:]:
-        # Check if this number is larger than the best one we have seen so far
         if number > best_so_far:
-            # This is the new best number, so save it to that variable
             best_so_far = number
 
     print(f"The largest number is: {best_so_far}")
-
-    ########################
-    # Enhancement
-    ########################
 
     smallest_so_far = None
 
     for number in numbers:
         if number > 0 and (smallest_so_far is None or number < smallest_so_far):
-            # We have a new smallest number
             smallest_so_far = number
 
     if smallest_so_far is None:
@@ -63,7 +52,6 @@ else:
     else:
         print(f"The smallest positive number is: {smallest_so_far}")
 
-# Sorting the list
 sorted_list = sorted(numbers)
 
 print("The sorted list is:")
